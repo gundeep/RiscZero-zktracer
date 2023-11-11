75a14c4c915992531aae0ca41abe000860643db4
gundeep <gundeep.upenn@gmail.com>
2023-11-11 09:41:44 +0100

    start with defining structs

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -7,11 +7,34 @@ use risc0_zkvm::guest::env;
 
 risc0_zkvm::guest::entry!(main);
 
+// Creating the structures for the scene
+pub struct Color {
+    pub red: f32,
+    pub green: f32,
+    pub blue: f32,
+}
+
+pub struct Sphere {
+    pub center: Point,
+    pub radius: f64,
+    pub color: Color,
+}
+
+pub struct Scene {
+    pub width: u32,
+    pub height: u32,
+    pub fov: f64,
+    pub sphere: Sphere,
+}
+
 pub fn main() {
     // TODO: Implement your guest code here
 
+
+
     // read the input
     let input: u32 = env::read();
+
 
     // TODO: do something with the input
 
---
+++ question.md
@@ -0,0 +1,1 @@
+what is the diff in main.rs and lib.rs?
